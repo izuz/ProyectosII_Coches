2d661b2d690dd933032196f5128ce88388547cc2
gon17 <g.on177@hotmail.com>
2022-03-07 19:19:02 +0100

    Estrcutura con bootsrap incluido

---
+++ public_html/js/cookies.js
@@ -0,0 +1,21 @@
+    
+        window.addEventListener("load", function(){
+        window.cookieconsent.initialise({
+          "palette": {
+            "popup": {
+              "background": "#efefef",
+              "text": "#404040"
+            },
+            "button": {
+              "background": "#d01732" //#3825c4
+            }
+          },
+          "theme": "classic",
+          "position": "bottom-left",
+          "content": {
+            "message": "Esta web utiliza cookies para obtener datos estadísticos de la navegación de sus usuarios. Si continúas navegando consideramos que aceptas su uso.",
+            "dismiss": "Cerrar",
+            "link": "Leer más",
+            "href": "https://www.aboutcookies.org/"
+          }
+        })});
